dfe7f95fcdd36711fe8acf0663263162c6f8d7e3
MarkoV <marek.kaszewski@gmail.com>
2019-01-08 21:25:56 +0000

    the main idea of the readings transmitter

--- EnvironmentMonitorNode/EnvironmentMonitorNode.ino
+++ EnvironmentMonitorNode/EnvironmentMonitorNode.ino
@@ -1,3 +1,17 @@
+//Measurments readings format:
+
+//T:-39.98;H:48.55;P:324523;B:3.2
+//T - temperature
+//H - humidity
+//P - pressure
+//B - battery level
+
+//message packet format
+//KS;01;T:-39.98;H:48.55;P:324523;B:3.2
+
+//encrypted message packet format
+//KS:01:<encrypted message packet format from above>
+
 #include <SPI.h>
 #include <LoRa.h>
 #include "SSD1306.h"
@@ -14,13 +28,13 @@
 
 //Logic configuration
 #define SEND_READINGS_DELAY_S 60
-#define DISPLAY_OFF_DELAY_S 60
+#define DISPLAY_OFF_DELAY_S 90
 
 //Endpoint identification
 //instalation identifier
 String netID = "KS";
 //endpoint identifier
-String nodeID = "02";
+String nodeID = "01";
 
 unsigned int counter = 0;
 int displayTimeout = 0;
@@ -28,16 +42,36 @@ int sendReadingsDelay = 0;
 
 SSD1306 display (0x3c, 4, 15);
 
+// Interrupt Service Routine - Keep it short!
+void IRAM_ATTR handleButtonInterrupt() {
+  if(displayTimeout == 0) {
+    displayTimeout = DISPLAY_OFF_DELAY_S;
+  }
+}
+
 String readSensors() {
-  String result = "T39.98H48.55P324523";
+  String result = "T:-39.98;H:48.55;P:324523;B:3.2";
   //read sensors here
   Serial.println ("Sensors: " + result);
-  return result;
+  return result; 
+}
+
+String preparePayload(String readings, bool encryptionEnabled) {
+  String result;
+  if(encryptionEnabled) {
+    return netID + ";" + nodeID + ";" + encryptPayload(readings);
+  } else {
+    return netID + ";" + nodeID + ";" + readings;
+  }
+}
+
+String encryptPayload(String payload) {
+  //no encryption for now
+  return payload;  
 }
 
 void transmitResults(String data) {
   digitalWrite (2, HIGH); // turn the LED on (HIGH is the voltage level)
-  // send packet
   LoRa.beginPacket ();
   LoRa.print (data);
   LoRa.endPacket ();
@@ -46,38 +80,66 @@ void transmitResults(String data) {
   LoRa.sleep();
 }
 
+String getValue(String data, String index) {
+    char string[] = "KS;01;T:-39.98;H:48.55;P:32453;B:3.2";
+    char delimiter[] = ";";
+    char delimiter2[] = ":";
+    char* ptr = strtok(string, delimiter);
+    int pointerIndex = 0;
+    while(ptr != NULL) {
+      if(index.equals("NET") && pointerIndex == 0) {
+        return String(ptr); 
+      } else if(index.equals("NODE") && pointerIndex == 1) {
+        return String(ptr); 
+      } else if(index.equals(String(ptr[0]))){
+        char* ptr2 = strtok(ptr, delimiter2);
+        while(ptr2 != NULL) {
+          if(!String(ptr2).equals(index)) return String(ptr2);
+          ptr2 = strtok(NULL, delimiter2);
+        }
+      }
+      ptr = strtok(NULL, delimiter);
+      pointerIndex++;
+    }
+    return "";
+}
+
 void displayResults(String data, bool txInd, int countdown) {
   display.clear();
   display.drawString(0, 0, "¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯");
   display.drawString(0, 2, "______________________");
   display.drawString(0, 4, "______________________");
   display.drawString(0, 0, "             |                 |       |");
-  display.drawString(0, 1, "NET:"+netID);
-  display.drawString(45, 1, "NODE:"+nodeID);
-  display.drawString(98, 1, String(countdown));
-  display.drawString(2, 17, "Temp");
-  display.drawString(57, 17, "[ºC]");
-  display.drawString(92, 17, "-26.89");
-  display.drawString(2, 27, "Humidity");
-  display.drawString(57, 27, "[%]"); 
-  display.drawString(92, 27, "58.65"); 
-  display.drawString(2, 37, "Pressure");
-  display.drawString(57, 37, "[hPa]");
-  display.drawString(92, 37, "1068"); 
-  //display.drawString(2, 47, "Dew point");
-  //display.drawString(57, 47, "[ºC]");
-  //display.drawString(92, 47, "-26.89");
+  if(!data.equals("")) {
+    display.drawString(0, 1, "NET:"+getValue(data, "NET"));
+    display.drawString(45, 1, "NODE:"+getValue(data, "NODE"));
+    display.drawString(98, 1, String(countdown));
+    display.drawString(4, 17, "Temp");
+    display.drawString(57, 17, "[ºC]");
+    display.drawString(92, 17, getValue(data, "T"));
+    display.drawString(4, 27, "Humidity");
+    display.drawString(57, 27, "[%]"); 
+    display.drawString(92, 27, getValue(data, "H"));   
+    display.drawString(4, 37, "Pressure");
+    display.drawString(57, 37, "[hPa]"); 
+    display.drawString(92, 37, getValue(data, "P"));   
+    display.drawString(4, 47, "Battery");
+    display.drawString(57, 47, "[V]");
+    display.drawString(92, 47, getValue(data, "B"));
+  } else {
+    display.drawString(0, 1, "NET:--");
+    display.drawString(45, 1, "NODE:--");
+  }
   display.drawString(0, 52, "______________________");
   display.drawLine(0,16,0, 62);
   display.drawLine(52,16,52, 62);
   display.drawLine(85,16,85, 62);
   display.drawLine(127,16,127, 62);
-//  display.drawString (0, 50, data);
   if(txInd) {
     display.drawCircle(124, 7, 3);
     display.drawCircle(124, 7, 1);
   }
-  display.display ();
+  display.display();
 }
 
 void setup () {
@@ -108,31 +170,42 @@ void setup () {
   display.flipScreenVertically();
   display.setFont(ArialMT_Plain_10);
   display.setTextAlignment(TEXT_ALIGN_LEFT);
-  
+
+  attachInterrupt(digitalPinToInterrupt(BUTTON), handleButtonInterrupt, FALLING);
+  displayResults("", false, 0);
   delay (1500);
 }
 
 void loop () {
-    Serial.println (displayTimeout);
-  if(!digitalRead(BUTTON)) {
-      displayTimeout = DISPLAY_OFF_DELAY_S;
+  String reading;
+  String displayData;
+  Serial.println ("cycle");
+  if(displayTimeout > 0 || sendReadingsDelay == 0) {
+    reading = readSensors();
   }
-  
-  String reading = readSensors();
   if(displayTimeout > 0) {
-    displayResults(reading, true, counter);
+    displayData = preparePayload(reading, false);
+    if(sendReadingsDelay == 0) {
+      displayResults(displayData, true, displayTimeout);
+    }
   }
-  transmitResults(reading);
+  if(sendReadingsDelay == 0) {
+    transmitResults(preparePayload(reading, true));
+    sendReadingsDelay = SEND_READINGS_DELAY_S;
+  }
   if(displayTimeout > 0) {
-    displayResults(reading, false, counter);
+    displayResults(displayData, false, displayTimeout);
     --displayTimeout; 
     if(displayTimeout == 0) {
       Serial.println ("clear display only once");
       display.clear();
-      display.display ();
+      display.display();
     }
+    delay (700);
+  } else {
+    delay (1000); 
   }
-  
+  sendReadingsDelay--;
   counter ++;
-  delay (1000); // wait for a second
+ // delay (1000); // wait for a second
 }
